77142847a2775bb92f6640d95624eacc1f2f99a1
EugeSorgetti <eugesorgetti@gmail.com>
2020-12-02 16:02:03 -0300

    Desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Navbar() {
+  return (
+    <nav className="navBar">
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Comprar</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
